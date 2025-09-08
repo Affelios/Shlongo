8d5444c77abe1a64de3ae07fe36ad1632c619f0b
Roger Creyke <creyke@gmail.com>
2025-09-08 15:37:23 +0100

    Added in ordering.

--- Shlongo/MongrationContext.cs
+++ Shlongo/MongrationContext.cs
@@ -7,6 +7,7 @@ namespace Shlongo
         public Mongration[] Mongrations { get; } = [.. mongrationAssembly
             .GetTypes()
             .Where(x => x.BaseType == typeof(Mongration))
-            .Select(x => (Mongration)Activator.CreateInstance(x)!)];
+            .Select(x => (Mongration)Activator.CreateInstance(x)!)
+            .OrderBy(x => int.Parse(x.GetType().Name.Split('_')[1]))];
     }
 }
