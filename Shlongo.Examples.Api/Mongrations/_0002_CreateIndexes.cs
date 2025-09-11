e635aaff66f0b56d2047491e19af7326439db25a
KieronAffelios <kieron@affelios.com>
2025-09-11 11:45:06 +0100

    Added create user and exception to example

--- Shlongo.Examples.Api/Mongrations/_0002_CreateIndexes.cs
+++ Shlongo.Examples.Api/Mongrations/_0002_CreateIndexes.cs
@@ -1,10 +1,21 @@
 ﻿namespace Shlongo.Examples.Api.Mongrations
 {
+    public class User
+    {
+        public string Name { get; set; }
+    }
+
     public class _0002_CreateIndexes : Mongration
     {
-        public override Task UpAsync(IMongrationContext context)
+        public override async Task UpAsync(IMongrationContext context)
         {
-            return Task.CompletedTask;
+            await context.Database.GetCollection<User>("Users").InsertManyAsync(new[]
+            {
+                new User { Name = "Alice" }
+            });
+
+            throw new Exception("Simulated failure during index creation.");
+            // return Task.CompletedTask; // Unreachable code, can be removed
         }
     }
 }
